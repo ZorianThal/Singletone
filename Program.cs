6ef7f5533302a86d22fc698ac887b8aa6f115ea8
Rei <Sugimotop8@gmail.com>
2026-09-18 17:58:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Settings
+{
+    private static Settings instance;
+    private Settings()
+    {
+
+    }
+
+    public static Settings GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Settings();
+        }
+        return instance;
+    }
+    public int Volime;
+}
+
+class Programm
+{
+    static void Main()
+    {
+        Settings settings1 = Settings.GetInstance();
+        Settings settings2 = Settings.GetInstance();
+
+        settings1.Volime = 80;
+
+        Console.WriteLine(settings1.Volime);
+    }
+}
